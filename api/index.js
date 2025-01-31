c1668fae816508c53814570d107c20c535a56eec
girish <girishgaikwad2055@gmail.com>
2025-01-31 07:04:12 +0530

    verce;

--- api/index.js
+++ api/index.js
@@ -447,6 +447,5 @@ app.post("/datewisesSubCategory", async (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+module.exports = app;
+
